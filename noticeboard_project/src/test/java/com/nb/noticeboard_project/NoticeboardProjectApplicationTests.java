57c58894275576a24f059d2f4dc78d21b78ab36a
DESKTOP-6M8EED2\hasci <https://github.com/HA-JIHYEOK>
2026-01-01 23:13:57 +0900

    스프링 환경 구축

---
+++ noticeboard_project/src/test/java/com/nb/noticeboard_project/NoticeboardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.noticeboard_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeboardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
